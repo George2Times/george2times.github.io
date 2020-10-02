49207e89704a6774e9548701209ef020d44cfc9f
George2Times <jerzy.dziubel@gmail.com>
2020-10-02 15:04:57 +0200

    add mouse click to piano keys

--- pianoScript.js
+++ pianoScript.js
@@ -12,3 +12,12 @@ document.addEventListener("keydown", function(event) {
         console.warn('Another key was pressed.');
     }
 });
+
+let keys = document.getElementsByTagName("kbd");
+for (let i=0; i<keys.length; i++) {
+    let key_name = keys[i].textContent;
+    keys[i].addEventListener("click", function(){
+        let audio = new Audio('res/'+ key_name +'.mp3');
+        audio.play().then(() => Node);
+    });
+}
